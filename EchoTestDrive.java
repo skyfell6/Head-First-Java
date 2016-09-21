16adac7774029ffe10c4f92f8d12440ecab69fec
Mike <totafell@gmail.com>
2016-09-20 20:47:11 -0400

    Update EchoTestDrive.java

--- EchoTestDrive.java
+++ EchoTestDrive.java
@@ -1,17 +1,20 @@
 public class EchoTestDrive{
    public static void main(String[] args){
       Echo e1 = new Echo();
-      e2 = e1;  // this won't compile right, I guess I haven't declared the e2 variable. 
+      Echo e2 = e1;  // this won't compile right, I guess I haven't declared the e2 variable. 
+                     // Looking into the answer.. Oh Echo e2=e1; is possible,  I thought it was not
+                     // SECOND POSSIBLE ANSWER for line 4: Echo e2 = new Echo();
       int x = 0;
-      while (x < 5){
+      while (x < 4){
          e1.hello();
+         e1.count = e1.count+1;  
          if(x==3){
             e2.count = e2.count+1;
          }
-         if (x>1){
+         if (x>0){
             e2.count = e2.count + e1.count;
          }
-      x = x+1;
+      x++;
       }
       System.out.println(e2.count);
    }
