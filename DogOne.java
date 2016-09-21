56302bce9e583cd413b9b5834d81d249f7dcd74c
Mike <totafell@gmail.com>
2016-09-21 16:28:31 -0400

    Update DogOne.java
    Doh me.. I wanted to keep my original Dog class file, so I named this class file "DogOne" and I still had the "Dog" reference variable named, and the compiler wouldn't declare all dog names except for Bart because it was looking into the Dog class!  Not DogOne class.  Now it works

--- DogOne.java
+++ DogOne.java
@@ -2,14 +2,14 @@ class DogOne{
    String name;
    public static void main(String[] args){      
    // make a new dog object and access it
-      Dog dog1 = new Dog();
+      DogOne dog1 = new DogOne();
       dog1.bark();
       dog1.name = "Bart";
    
       // now make a dog array
-      Dog[] myDogs = new Dog[3];
-      myDogs[0] = new Dog();
-      myDogs[1] = new Dog();
+      DogOne[] myDogs = new DogOne[3];
+      myDogs[0] = new DogOne();
+      myDogs[1] = new DogOne();
       myDogs[2] = dog1;
    
       myDogs[0].name = "Fred";
