f35377511d304884a7a12a5de3165c9e116d41b0
Mike <totafell@gmail.com>
2016-09-20 20:25:13 -0400

    Update and rename DrumKitTestDrive to DrumKitTestDrive.java

--- DrumKitTestDrive.java
+++ DrumKitTestDrive.java
@@ -1,11 +1,13 @@
 public class DrumKitTestDrive{
    public static void main(String[] args){
+   
       DrumKit d = new DrumKit();
-      if (d.snare==true){  //original code: d.snare==true (== >> !=)
+      d.playSnare();
+      d.snare=false;
+      d.playTopHat();
+      
+      if (d.snare==true){
          d.playSnare();         
       }
-      d.snare=false;
-      d.playSnare();
-      d.playTopHat();   
    }
 }
