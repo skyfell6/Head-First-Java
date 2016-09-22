4ff4810f4d85e5f18904457f07d8ab92fb2ab124
Mike <totafell@gmail.com>
2016-09-22 16:01:40 -0400

    Add files via upload
    My first attempt at making the snippets in order, won't compile right.

---
+++ TestArrays.java
@@ -0,0 +1,25 @@
+// Code magnet page 64  (placing code snippets in order) 9/22/2016
+class TestArrays{
+   public static void main(String[] args){
+      String [] islands = new String[4];
+      islands[0] = "Bermuda";
+      islands[1] = "Figi";
+      islands[2] = "Azores";
+      islands[3] = "Cozumel";
+   
+      int ref;
+      int y = 0;
+   
+      while (y<4){
+         ref = index[y];
+         int [] index = new int[4];
+         index[0] = 1;
+         index[1] = 3;
+         index[2] = 0;
+         index[3] = 2;
+      System.out.print("island = ");
+      System.out.println(islands[ref]);
+      }
+      y++;  // y = y+1;
+   }
+}
