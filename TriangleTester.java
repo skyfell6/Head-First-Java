24a58c4a6b42e6a60e03bdd16b2ef3cc3a519e1d
Mike <skyfell6@users.noreply.github.com>
2016-10-06 17:50:19 -0400

    Update TriangleTester.java

--- TriangleTester.java
+++ TriangleTester.java
@@ -1,18 +1,21 @@
 // page 65 Date: 10/6/16
-public class TriangleTester {
+public class Triangle {
+   double area;
+   int height;
+   int length;
    public static void main(String[] args){
       int x = 0;
       Triangle [] ta = new Triangle[4];
       while (x<4){
-         ta[x].area
+         ta[x] = new Triangle();
          ta[x].height = (x+1) * 2;
          ta[x].length = x+4;
-         ta[x].area
+         ta[x].setArea();
          System.out.print("triangle "+x+", area");
          System.out.println(" = "+ta[x].area);
          x = x+1;
       }
-      int x;
+      int y = x;
       x=27;
       Triangle t5 = ta[2];
       ta[2].area = 343;
