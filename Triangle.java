2a6c7739556b80348a90b13784980b3c3c60eeca
Mike <skyfell6@users.noreply.github.com>
2016-10-06 17:53:23 -0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -4,18 +4,18 @@ public class Triangle {
    int height;
    int length;
    public static void main(String[] args){
-      int x = 0;
-      Triangle [] ta = new Triangle[4];
-      while (x<4){
-         ta[x] = new Triangle();
+      int x = 0;                               // correct!
+      Triangle [] ta = new Triangle[4];        // correct!
+      while (x<4){                             // correct!
+         ta[x] = new Triangle();               // was wrong, now correct!
          ta[x].height = (x+1) * 2;
          ta[x].length = x+4;
-         ta[x].setArea();
+         ta[x].setArea();                      // was wrong, now correct!
          System.out.print("triangle "+x+", area");
-         System.out.println(" = "+ta[x].area);
+         System.out.println(" = "+ta[x].area); // correct!
          x = x+1;
       }
-      int y = x;
+      int y = x;                               // was wrong, now correct!
       x=27;
       Triangle t5 = ta[2];
       ta[2].area = 343;
@@ -23,6 +23,6 @@ public class Triangle {
       System.out.println(", t5 area = "+ t5.area);
    }
    void setArea() {
-      area=(height*length)/2;
+      area=(height*length)/2;                  // correct!
    }
 }
