08bbc634d14246ee7ec7655bd09aedfb39880759
Mike <skyfell6@users.noreply.github.com>
2016-10-21 16:00:39 -0400

    Update Puzzle4.java

--- Puzzle4.java
+++ Puzzle4.java
@@ -5,7 +5,7 @@
 
 public class Puzzle4{
    public static void main(String[] args){
-      Puzzle4[] obs = new Puzzle4[6];
+      Puzzle4b[] obs = new Puzzle4b[6];
       int y = 1;
       int x = 0;
       int result = 0;
